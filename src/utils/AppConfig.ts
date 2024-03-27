2401325d57bd30c39b5e272ce1382c22c8d89404
chiyun <onlymipa@hotmail.com>
2024-03-28 02:05:06 +0800

    update

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'React landing page',
-  title: 'React landing page template 2021',
-  description: 'Production ready plug n play landing page!',
-  locale: 'en',
+  site_name: '萝拉酱 - FGO 异世界女仆 ',
+  title: 'FGO 女仆代肝 | 清体力 | 搓丸子 | 过剧情 ',
+  description: '养肝, 护肝',
+  locale: 'zh-cn',
 };
